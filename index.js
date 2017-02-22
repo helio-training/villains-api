697fe55646f939a0d2717ac37b9517ff405a6466
Tyler Garlick <tjgarlick@gmail.com>
2017-02-22 16:13:05 -0700

    dist

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ require('babel-polyfill');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-// const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV;
 
-// module.exports = require('./dist');
 
-require('babel-register');
-
-module.exports = require('./src');
-
-// if (env === 'production') {
-//   module.exports = require('./dist');
-// } else {
-//   require('babel-register');
-//   module.exports = require('./src');
-// }
+if (env === 'production') {
+  module.exports = require('./dist');
+} else {
+  require('babel-register');
+  module.exports = require('./src');
+}
 
